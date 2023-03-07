74c2609a20d02586171281e17431c46e12e49035
thuangiovn <111517437+thuangiovn@users.noreply.github.com>
2023-03-07 10:46:53 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import solid from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
+  site: 'https://6406b3425671d226c8ab63c0--vermillion-mochi-c1369f.netlify.app',
   integrations: [solid()]
 });
